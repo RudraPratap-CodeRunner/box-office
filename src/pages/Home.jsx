ce04e45edc31cba8aeaa9cabb0e658e5d0eb80d2
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-05-16 18:43:52 +0530

    fetch and display data from tvmaze api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import { searchForShows } from "./../api/tvmaze";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [searchStr, setsearchstr] = useState("");
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
 
-export default Home
+  const onSearchInputChange = (ev) => {
+    setsearchstr(ev.target.value);
+  };
+
+  const onSearch = async (ev) => {
+    ev.preventDefault();
+    try {
+      setApiDataError(null);
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setApiDataError(error);
+    }
+  };
+
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error occured! {apiDataError.message}</div>;
+    }
+    if (apiData) {
+      return apiData.map((data) => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+    return null;
+  };
+
+  return (
+    <div>
+      <form action="" onSubmit={onSearch}>
+        <input type="text" onChange={onSearchInputChange} value={searchStr} />
+        <button type="submit">Search</button>
+      </form>
+      <div>{renderApiData()}</div>
+    </div>
+  );
+};
+
+export default Home;
