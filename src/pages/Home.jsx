637aa07c4b7ce876618acb25da3d8d362b788298
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-05-20 13:01:30 +0530

    used react-query on Home page for fetching data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,44 @@
 import React, { useState } from "react";
-import { searchForShows,searchForPeople } from "./../api/tvmaze";
+import { searchForShows, searchForPeople } from "./../api/tvmaze";
 import SearchForm from "../components/SearchForm";
 import ShowGrid from "../components/shows/ShowGrid";
 import ActorsGrid from "../components/actors/ActorsGrid";
+import { useQuery } from "@tanstack/react-query";
 
 const Home = () => {
+  const [filter, setFilter] = React.useState(null);
 
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
- 
-  
+  const { data: apiData,error:apiDataError } = useQuery({
+    queryKey: ["search", filter],
+    queryFn: () =>
+      filter.searchOption === "shows"
+        ? searchForShows(filter.q)
+        : searchForPeople(filter.q),
+    // ⬇️ disabled as long as the filter is empty
+    enabled: !!filter,
+    refetchOnWindowFocus:false
+  });
 
- 
 
-  const onSearch = async ({q,searchOption}) => {
- 
-    try {
-      setApiDataError(null);
-      let result;
-      if(searchOption==='shows'){
-        result = await searchForShows(q);
-      
-      }else{
-        result = await searchForPeople(q);
-        
-      }
-      setApiData(result);
-    } catch (error) {
-      setApiDataError(error);
-    }
+
+  const onSearch = async ({ q, searchOption }) => {
+    setFilter({ q, searchOption });
+
   };
 
   const renderApiData = () => {
     if (apiDataError) {
       return <div>Error occured! {apiDataError.message}</div>;
     }
-    if(apiData?.length===0){
-      return <div>No results</div>
+    if (apiData?.length === 0) {
+      return <div>No results</div>;
     }
     if (apiData) {
-      return apiData[0].show ? <ShowGrid shows={apiData} /> :<ActorsGrid actors={apiData} />
-      
+      return apiData[0].show ? (
+        <ShowGrid shows={apiData} />
+      ) : (
+        <ActorsGrid actors={apiData} />
+      );
     }
     return null;
   };
@@ -48,7 +46,7 @@ const Home = () => {
   return (
     <div>
       <SearchForm onSearch={onSearch} />
-     
+
       <div>{renderApiData()}</div>
     </div>
   );
