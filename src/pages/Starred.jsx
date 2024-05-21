c04bf89b67d61fce51956d962f9bfd94d16eaca1
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-05-21 10:57:30 +0530

    displayed starred shows in Starred page

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import { useStarredShows } from '../lib/useStarredShows';
+import { useQuery } from '@tanstack/react-query';
+import { getShowById, getShowByIds } from '../api/tvmaze';
+import ShowGrid from '../components/shows/ShowGrid';
 
 const Starred = () => {
-  const [starredShows] = useStarredShows();
+  const [starredShowsIds] = useStarredShows();
+
+  const { data: starredShows,error:starredShowsError } = useQuery({
+    queryKey:  ["starred", starredShowsIds],
+    queryFn:async () =>getShowByIds(starredShowsIds).then(result=>result.map(show =>({show }))),
+      
+
+    refetchOnWindowFocus:false
+  });
+
+  if(starredShows?.length===0){
+    return <div>No shows were starred</div>
+  }
+
+  if(starredShows){
+    return <ShowGrid shows={starredShows}/>
+  }
+  if(starredShowsError){
+    return <div>Error occured:{starredShowsError.message}</div>
+  }
 
   return (
-    <div>Starred, starred page {starredShows.length}</div>
+    <div>Data still loading</div>
   )
 }
 
